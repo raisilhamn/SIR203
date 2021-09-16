a99ffde62b0455cabeba91461aa988f1aa86fef5
Rais Ilham <raispinter12@gmail.com>
2021-09-16 15:38:12 +0700

    update minggu ke 2

--- tugas 2/Main.java
+++ tugas 2/Main.java
@@ -6,42 +6,39 @@ package latihan2;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Pecahan p1 = new Pecahan();
+    p1.setPembilang(1);
+    p1.setPenyebut(2);
+    Pecahan p2 = new Pecahan();
+    p2.setPembilang(1);
+    p2.setPenyebut(2);
 
-        Pecahan p1 = new Pecahan();
-        p1.setPembilang(1);
-        p1.setPenyebut(2);
-        Pecahan p2 = new Pecahan();
-        p2.setPembilang(1);
-        p2.setPenyebut(2);
+    System.out.print(p1.getPembilang() + "/" + p1.getPenyebut());
+    System.out.print(" + ");
+    System.out.print(p2.getPembilang() + "/" + p2.getPenyebut());
+    System.out.print(" = ");
 
-        System.out.print(p1.getPembilang() + "/" + p1.getPenyebut());
-        System.out.print(" + ");
-        System.out.print(p2.getPembilang() + "/" + p2.getPenyebut());
-        System.out.print(" = ");
+    Pecahan hasil;
+    hasil = Pecahan.tambah(p1, p2);
+    System.out.println(hasil.getPembilang() + "/" + hasil.getPenyebut());
 
-        Pecahan hasil;
-        hasil = Pecahan.tambah(p1, p2);
-        System.out.println(hasil.getPembilang() + "/" + hasil.getPenyebut());
+    Pecahan p3 = new Pecahan();
+    p3.setPembilang(90);
+    p3.setPenyebut(95);
+    Pecahan p4 = new Pecahan();
+    p4.setPembilang(5);
+    p4.setPenyebut(10);
+    System.out.print(p3.getPembilang() + "/" + p3.getPenyebut());
+    System.out.print(" * ");
+    System.out.print(p4.getPembilang() + "/" + p4.getPenyebut());
+    System.out.print(" = ");
 
-        Pecahan p3 = new Pecahan();
-        p3.setPembilang(90);
-        p3.setPenyebut(95);
-        Pecahan p4 = new Pecahan();
-        p4.setPembilang(5);
-        p4.setPenyebut(10);
-        System.out.print(p3.getPembilang() + "/" + p3.getPenyebut());
-        System.out.print(" * ");
-        System.out.print(p4.getPembilang() + "/" + p4.getPenyebut());
-        System.out.print(" = ");
-
-        Pecahan hasil2;
-        hasil2 = Pecahan.kali(p3, p4);
-        System.out.println(hasil2.getPembilang() + "/" + hasil2.getPenyebut());
-
-    }
-
+    Pecahan hasil2;
+    hasil2 = Pecahan.kali(p3, p4);
+    System.out.println(hasil2.getPembilang() + "/" + hasil2.getPenyebut());
+  }
 }
--- tugas 2/Pecahan.java
+++ tugas 2/Pecahan.java
@@ -11,65 +11,61 @@ package latihan2;
  */
 public class Pecahan {
 
-    private int pembilang;
-    private int penyebut;
+  private int pembilang;
+  private int penyebut;
 
-    public Pecahan(int pembilang, int penyebut) {
-        this.pembilang = pembilang;
-        this.penyebut = penyebut;
+  public Pecahan(int pembilang, int penyebut) {
+    this.pembilang = pembilang;
+    this.penyebut = penyebut;
+  }
+
+  public Pecahan() {}
+
+  // mencari FPB untuk menyederhanakan
+  public static int FPB(int a, int b) {
+    while (b != 0) {
+      int temp = a;
+      a = b;
+      b = temp % b;
     }
+    return a;
+  }
 
-    public Pecahan() {
-        
-        
-    }
+  public int getPembilang() {
+    return this.pembilang;
+  }
 
-    // mencari FPB untuk menyederhanakan 
-    public static int FPB(int a, int b) {
-        while (b != 0) {
-            int temp = a;
-            a = b;
-            b = temp % b;
-        }
-        return a;
-    }
+  public void setPembilang(int pembilang) {
+    this.pembilang = pembilang;
+  }
 
-    public int getPembilang() {
-        return this.pembilang;
-    }
+  public void setPenyebut(int penyebut) {
+    this.penyebut = penyebut;
+  }
 
-    public void setPembilang(int pembilang) {
-        this.pembilang = pembilang;
-    }
+  public int getPenyebut() {
+    return this.penyebut;
+  }
 
-    public void setPenyebut(int penyebut) {
-        this.penyebut = penyebut;
-    }
+  public static Pecahan tambah(Pecahan p1, Pecahan p2) {
+    int penyebutSetara = p1.getPenyebut() * p2.getPenyebut();
+    int pembilangP1 = p1.getPenyebut() * p2.getPembilang();
+    int pembilangP2 = p1.getPembilang() * p2.getPenyebut();
+    int pembilangAkhir = pembilangP1 + pembilangP2;
+    int fpb = FPB(penyebutSetara, pembilangAkhir);
+    penyebutSetara /= fpb;
+    pembilangAkhir /= fpb;
+    Pecahan pecahanAkhir = new Pecahan(pembilangAkhir, penyebutSetara);
+    return pecahanAkhir;
+  }
 
-    public int getPenyebut() {
-        return this.penyebut;
-    }
-
-    public static Pecahan tambah(Pecahan p1, Pecahan p2) {
-        int penyebutSetara = p1.getPenyebut() * p2.getPenyebut();
-        int pembilangP1 = p1.getPenyebut() * p2.getPembilang();
-        int pembilangP2 = p1.getPembilang() * p2.getPenyebut();
-        int pembilangAkhir = pembilangP1 + pembilangP2;
-        int fpb = FPB(penyebutSetara, pembilangAkhir);
-        penyebutSetara /= fpb;
-        pembilangAkhir /= fpb;
-        Pecahan pecahanAkhir = new Pecahan(pembilangAkhir, penyebutSetara);
-        return pecahanAkhir;
-    }
-
-    public static Pecahan kali(Pecahan p1, Pecahan p2) {
-        int penyebutSetara = p1.getPenyebut() * p2.getPenyebut();
-        int pembilangAkhir = p1.getPembilang() * p2.getPembilang();
-        int fpb = FPB(penyebutSetara, pembilangAkhir);
-        penyebutSetara /= fpb;
-        pembilangAkhir /= fpb;
-        Pecahan pecahanAkhir = new Pecahan(pembilangAkhir, penyebutSetara);
-        return pecahanAkhir;
-    }
-
+  public static Pecahan kali(Pecahan p1, Pecahan p2) {
+    int penyebutSetara = p1.getPenyebut() * p2.getPenyebut();
+    int pembilangAkhir = p1.getPembilang() * p2.getPembilang();
+    int fpb = FPB(penyebutSetara, pembilangAkhir);
+    penyebutSetara /= fpb;
+    pembilangAkhir /= fpb;
+    Pecahan pecahanAkhir = new Pecahan(pembilangAkhir, penyebutSetara);
+    return pecahanAkhir;
+  }
 }
