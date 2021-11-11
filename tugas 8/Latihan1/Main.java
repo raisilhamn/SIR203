ef84c14338a0a7b78e9186c7b0cec809d589c397
Rais Ilham <raispinter12@gmail.com>
2021-11-11 12:48:41 +0700

    memperbaiki nama

--- tugas 8/Latihan1/Main.java
+++ tugas 8/Latihan1/Main.java
@@ -4,7 +4,7 @@ public class Main{
     
     public static void main(String[] args) {
         DoraMini dm = new DoraMini();
-        dm.setNama("nama saya dora mini");
+        dm.setNama("dora mini");
         dm.setTahunPembuatan(2020);
         dm.displayData();
         dm.sayDora();
