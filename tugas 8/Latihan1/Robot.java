aec186edd595c1a3458d2d1ac274323d7dc98c6e
Rais Ilham <raispinter12@gmail.com>
2021-11-11 12:34:31 +0700

    update

--- tugas 8/Latihan1/Robot.java
+++ tugas 8/Latihan1/Robot.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mingguke8.Latihan1;
 
-/**
- *
- * @author LENOVO
- */
 public abstract class Robot {
     protected String nama;
     protected String pemilik;
